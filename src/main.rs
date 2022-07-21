6b19df598916b4e46abb216df14eed2ec1396413
Sanjay <30881+sanjayts@users.noreply.github.com>
2022-07-21 16:01:48 +0100

    Fix clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env::args;
 use std::process::exit;
 
 fn main() {
-    let cmd_args = args().map(|s| s.to_owned()).collect();
+    let cmd_args = args().collect();
     if let Err(e) = parse_config(cmd_args).and_then(run) {
         eprintln!("{}", e);
         exit(1);
